5662a1b00541c6dec88f578af424c8a1cdb3034a
Lucas Ausberger <ausbergerl@hotmail.com>
2022-02-22 17:03:46 -0600

    cookie header is now reassembled

--- Burp Prototype/src/burp/BurpExtender.java
+++ Burp Prototype/src/burp/BurpExtender.java
@@ -2,6 +2,7 @@ package burp;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,17 +37,18 @@ public class BurpExtender implements IBurpExtender, IHttpListener {
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
         if (messageIsRequest) { // Is the packet a HTTP request coming from the client?
             IRequestInfo request = this.helpers.analyzeRequest(messageInfo.getHttpService(), messageInfo.getRequest());
+            List<String> request_headers = request.getHeaders();
 
-            // Some definitions:
-            // Field: The entire line 
-
-            for (String header : request.getHeaders()) { // Here, each header is defined as a field in the packet, ex. 'User-Agent: ...'
+            for (String header : request_headers) { // Here, each header is defined as a field in the packet, ex. 'User-Agent: ...'
                 String header_name = header.split(":")[0]; // The first word before the semicolon will be the header's name
+
                 if (header_name.equals("Cookie")) { // For now, we only look at ones specifically referred to as a cookie
                     this.cookieCounter++;
                     //debug.println(header); // uncomment to view only the cookies
 
                     String[] cookie_contents = header.split(" "); // get a list of each space-separated word in the cookie header
+                    debug.println("Original cookie: " + Arrays.toString(cookie_contents)); 
+
                     for (int i=1; i<cookie_contents.length; i++) { // iterate through these words, skipping the first entry, "Cookie:"
                         
                         // Would be nice to have a Regex that can tolerate fields such as:
@@ -84,16 +86,27 @@ public class BurpExtender implements IBurpExtender, IHttpListener {
 
                         scrambledCookieEntry += scrambledValue; // add value back onto its key(s)
 
-                        debug.println("Original cookie field: " + cookie_entry);
-                        debug.println("Scrambled cookie field: " + scrambledCookieEntry);
-                        debug.println();
+                        //debug.println("Original cookie field: " + cookie_entry);
+                        //debug.println("Scrambled cookie field: " + scrambledCookieEntry);
+                        //debug.println();
 
-                        String newHeader = header.concat("\n" + scrambledCookieEntry + "\n TEST TEST TEST TEST");
+                        String newHeader = header.concat("\n" + scrambledCookieEntry);
                         //System.out.println(scrambledCookieEntry);
                         //debug.println(newHeader);
+                        cookie_contents[i] = scrambledCookieEntry; // swap the original entry with the one we've manipulated
                     }
+                    //debug.println("Modified cookie: " + Arrays.toString(cookie_contents));
+                    String reassembled_cookie = "";
+                    
+                    for (String entry : cookie_contents) { // turn our list of cookie components back into a single string
+                        reassembled_cookie += entry + " ";
+                    }
+
+                    reassembled_cookie = reassembled_cookie.substring(0, reassembled_cookie.length()-1); // remove extra space at end
+                    debug.println("Modified cookie: " + reassembled_cookie);
+
                 }
-                //debug.println(header); // uncomment to see all of the HTTP response packet
+                //debug.println(header); // uncomment to see all of the HTTP response packet     
             }
 
         } else { // to work with HTTP responses sent by the website/server and received by our computer
