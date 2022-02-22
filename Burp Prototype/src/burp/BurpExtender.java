0101bdd902ee512717ad2bae12af520e7a3278d1
Lucas Ausberger <ausbergerl@hotmail.com>
2022-02-22 16:25:28 -0600

    renamed variables for clarity

--- Burp Prototype/src/burp/BurpExtender.java
+++ Burp Prototype/src/burp/BurpExtender.java
@@ -36,55 +36,61 @@ public class BurpExtender implements IBurpExtender, IHttpListener {
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
         if (messageIsRequest) { // Is the packet a HTTP request coming from the client?
             IRequestInfo request = this.helpers.analyzeRequest(messageInfo.getHttpService(), messageInfo.getRequest());
+
+            // Some definitions:
+            // Field: The entire line 
+
             for (String header : request.getHeaders()) { // Here, each header is defined as a field in the packet, ex. 'User-Agent: ...'
-                String headerField = header.split(":")[0]; // Separate what's before and after the semicolon to get a [Header, Data] pair
-                if (headerField.equals("Cookie")) { // For now, we only look at ones specifically referred to as a cookie
-                    
+                String header_name = header.split(":")[0]; // The first word before the semicolon will be the header's name
+                if (header_name.equals("Cookie")) { // For now, we only look at ones specifically referred to as a cookie
                     this.cookieCounter++;
                     //debug.println(header); // uncomment to view only the cookies
 
-                    String[] cookieFields = header.split(" "); // get a list of each separate word in the header
-                    
-                    for (int i=1; i<cookieFields.length; i++) { // iterate through these words, skipping the first entry, "Cookie: "
-                        /*
-                        Would be nice to have a Regex that can tolerate fields such as:
-                            "Trk0=Value=1483300&Creation=13%2f02%2f2022+02%3a55%3a06"
-                        Ideally, only the value after the last "=" would be grabbed.
-                        Then, we can skip checking for the word "Cookie" and instead focus on wherever a key=value pair is assigned
-                        */
-                        String cookieField = cookieFields[i];
-                        String[] keyValueSets = cookieField.split("="); // Get a list of each key or value within the cookie field
-                        String reassembledCookieField = "";
+                    String[] cookie_contents = header.split(" "); // get a list of each space-separated word in the cookie header
+                    for (int i=1; i<cookie_contents.length; i++) { // iterate through these words, skipping the first entry, "Cookie:"
+                        
+                        // Would be nice to have a Regex that can tolerate fields such as:
+                        //     "Trk0=Value=1483300&Creation=13%2f02%2f2022+02%3a55%3a06"
+                        // Ideally, only the value after the last "=" would be grabbed.
+                        // Then, we can skip checking for the word "Cookie" and instead focus on wherever a key=value pair is assigned
 
-                        for (int j=0; j<keyValueSets.length-1; j++) { // re-add the key(s), but not the value, to our replacement cookie
-                            reassembledCookieField += keyValueSets[j] + "=";
+                        String cookie_entry = cookie_contents[i];
+                        String[] keyValueSet = cookie_entry.split("="); // Get a list of the key(s) and value within the cookie entry
+                        String scrambledCookieEntry = "";
+                        
+                        for (int j=0; j<keyValueSet.length-1; j++) { // re-add everything leading up to the value to our replacement cookie
+                            scrambledCookieEntry += keyValueSet[j] + "=";
                         }
                         
-                        String valueToScramble = keyValueSets[keyValueSets.length-1].split(";")[0]; // cookie fields are separated by semicolons
+                        String cookie_entry_value = keyValueSet[keyValueSet.length-1].split(";")[0]; // cookie entries are separated by semicolons
                         
                         // Here's a bunch of Java bullshit. All it does is rearrange the set of characters in the value section
-                        ArrayList<Character> scrambleChars = new ArrayList<Character>(valueToScramble.length());
-                        for (char c : valueToScramble.toCharArray()) {
-                            scrambleChars.add(c);
+                        ArrayList<Character> scrambledChars = new ArrayList<Character>(cookie_entry_value.length());
+                        for (char c : cookie_entry_value.toCharArray()) {
+                            scrambledChars.add(c);
                         }
 
-                        Collections.shuffle(scrambleChars);
-                        char[] shuffled = new char[scrambleChars.size()];
+                        Collections.shuffle(scrambledChars);
+                        char[] shuffled = new char[scrambledChars.size()];
 
                         for (int k=0; k<shuffled.length; k++) {
-                            shuffled[k] = scrambleChars.get(k);
+                            shuffled[k] = scrambledChars.get(k);
                         }
 
                         String scrambledValue = new String(shuffled);
-                        if (i < cookieFields.length-1) { // the last key/value doesn't have a semicolon
+                        if (i < cookie_contents.length-1) { // the last entry doesn't end with a semicolon
                             scrambledValue += ";";
                         }
-                        reassembledCookieField += scrambledValue; // add value back onto its key(s)
-                        ///*
-                        debug.println("Original cookie field: " + cookieField);
-                        debug.println("Scrambled cookie field: " + reassembledCookieField);
+
+                        scrambledCookieEntry += scrambledValue; // add value back onto its key(s)
+
+                        debug.println("Original cookie field: " + cookie_entry);
+                        debug.println("Scrambled cookie field: " + scrambledCookieEntry);
                         debug.println();
-                        //*/
+
+                        String newHeader = header.concat("\n" + scrambledCookieEntry + "\n TEST TEST TEST TEST");
+                        //System.out.println(scrambledCookieEntry);
+                        //debug.println(newHeader);
                     }
                 }
                 //debug.println(header); // uncomment to see all of the HTTP response packet
@@ -96,7 +102,7 @@ public class BurpExtender implements IBurpExtender, IHttpListener {
                 String field = sr.split(":")[0];
                 if (field.equals("Set-Cookie")) { // HTTP responses are what create cookies in the first place. 
                     this.cookieCounter++;
-                    debug.println(sr);
+                    //debug.println(sr);
                 }
             }
             // Get all cookies as an object if we want to
